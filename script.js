f10bf5660ef31df71caec7123fc924a99759e0db
William Finocchiaro <89465648+WILLIAMFINOCCHIARO@users.noreply.github.com>
2021-11-12 12:12:01 +0100

    jsnak2

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
-
-const utente = parseInt(prompt("metti in numero"));
-let utentedue = parseInt(prompt(" metti un numero"));
-
-
-if ( utente > utentedue ) {
-  console.log(utente);
-}
-else if  ( utente < utentedue ) {
-  console.log(utentedue);
-}
+// jsnack2 - 15 minuti
+// Il software deve chiedere per 10 volte all’utente di inserire un numero.
+// Il programma stampa la somma di tutti i numeri inseriti.
 
 
 
+// let somma =0;
+// let n;
 
+// for (i = 0; i< 10; i++) {
+//   const n = parseInt(prompt ("inserisci un numero"));
+//   somma+=n;
+// }
 
+// console.log(somma);
