0236295486a36da4df81596bfe479a69ac8eee69
William Finocchiaro <89465648+WILLIAMFINOCCHIARO@users.noreply.github.com>
2021-11-12 11:35:07 +0100

    complete

---
+++ script.js
@@ -0,0 +1,16 @@
+
+const utente = parseInt(prompt("metti in numero"));
+let utentedue = parseInt(prompt(" metti un numero"));
+
+
+if ( utente > utentedue ) {
+  console.log(utente);
+}
+else if  ( utente < utentedue ) {
+  console.log(utentedue);
+}
+
+
+
+
+
